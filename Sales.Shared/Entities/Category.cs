527324eeb02ffaa0f637f09711fce49d4f03a5b2
anderson472 <jagarcia472@misena.edu.co>
2023-03-03 04:49:44 -0500

    Begin initial form category

---
+++ Sales.Shared/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Categoría")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caractéres")]
+        public string Name { get; set; } = null!;
+
+       // public ICollection<State>? States { get; set; }
+
+        //public int StatesNumber => States == null ? 0 : States.Count;
+    }
+}
